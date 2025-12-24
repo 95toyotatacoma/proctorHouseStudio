1919d8cfcc46317d520442b4df016b94576e1672
Rina.P <iamrina.p@gmail.com>
2025-12-24 13:53:21 -0600

    fixed projects nav links and styling, classes cause error when updated the tsx

--- src/components/navigation/HomeProjectsNav.tsx
+++ src/components/navigation/HomeProjectsNav.tsx
@@ -1,4 +1,5 @@
 // src/components/navigation/HomeProjectsNav.tsx
+import React from "react";
 import { Link } from "react-router-dom";
 
 type ProjectLink = {
@@ -9,40 +10,20 @@ type ProjectLink = {
 };
 
 const PROJECT_LINKS: ProjectLink[] = [
-  {
-    id: "tomy-2000",
-    label: "Tomy 2000",
-    to: "/projects/tomy-2000",
-    imageFile: "front-side-redesign-idea.png",
-  },
-  {
-    id: "the-book",
-    label: "The Book",
-    to: "/projects/the-book",
-    imageFile: "book-cover.png",
-  },
-  {
-    id: "life-cards",
-    label: "Life Cards",
-    to: "/projects/life-cards",
-    imageFile: "life-cards.png",
-  },
+  { id: "tomy-2000", label: "Tomy 2000", to: "/projects/tomy-2000", imageFile: "front-side-redesign-idea.png" },
+  { id: "the-book", label: "The Book", to: "/projects/the-book", imageFile: "the-book.jpeg" },
+  { id: "life-cards", label: "Life Cards", to: "/projects/life-cards", imageFile: "life-cards.jpeg" },
 ];
 
 const HomeProjectsNav: React.FC = () => {
   return (
-    <nav
-      className="home-projects-nav__link"
-      aria-label="Core Proctor House Studio projects"
-    >
+    <nav className="home-projects-nav" aria-label="Core Proctor House Studio projects">
       <div className="home-projects-nav__inner">
         <ul className="home-projects-nav__list">
           {PROJECT_LINKS.map((project) => (
             <li key={project.id} className="home-projects-nav__item">
               <Link to={project.to} className="home-projects-nav__link">
-                {/* image + label */}
-              
-                <span className="home-projects-nav__image-wrap">
+                <span className="home-projects-nav__image-wrap" aria-hidden="true">
                   <span
                     className="home-projects-nav__image"
                     style={{
@@ -50,13 +31,10 @@ const HomeProjectsNav: React.FC = () => {
                       backgroundSize: "cover",
                       backgroundPosition: "center",
                     }}
-                    role="presentation"
-                    aria-hidden="true"
                   />
                 </span>
-                <span className="home-projects-nav__label">
-                  {project.label}
-                </span>
+
+                <span className="home-projects-nav__label">{project.label}</span>
               </Link>
             </li>
           ))}
